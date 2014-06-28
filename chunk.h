a6b5ef2f0ece1a555bd44b1fa9bf86483bc864c5
(no author) <(no author)@d8a302eb-03bc-478d-80e4-98257eca68ef>
2014-06-28 20:30:27 +0000

    chunk
    git-svn-id: svn://elaine/murooma/trunk@25 d8a302eb-03bc-478d-80e4-98257eca68ef

---
+++ chunk.h
@@ -0,0 +1,20 @@
+#ifndef CHUNK_H
+#define CHUNK_H
+
+#define MS (50)
+//44100 / 20 = 2205
+#define SAMPLE_RATE (44100)
+#define SIZE (SAMPLE_RATE*4*MS/1000)
+int bufferMs;
+#define FRAMES_PER_BUFFER  (SIZE/4)
+
+
+struct Chunk
+{
+	int32_t tv_sec;
+	int32_t tv_usec;
+	char payload[SIZE];
+};
+
+#endif
+
--- client.cpp
+++ client.cpp
@@ -17,22 +17,8 @@
 #include <mutex>
 #include <condition_variable>
 #include <portaudio.h>
+#include "chunk.h"
 
-
-#define MS (50)
-//44100 / 20 = 2205
-#define SAMPLE_RATE (44100)
-#define SIZE (SAMPLE_RATE*4*MS/1000)
-int bufferMs;
-#define FRAMES_PER_BUFFER  (SIZE/4)
-
-
-struct Chunk
-{
-	int32_t tv_sec;
-	int32_t tv_usec;
-	char payload[SIZE];
-};
 
 std::deque<Chunk*> chunks;
 std::deque<int> timeDiffs;
