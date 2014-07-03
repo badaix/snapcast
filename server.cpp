974174d181b9775ced6593f56712b1414478f56e
(no author) <(no author)@d8a302eb-03bc-478d-80e4-98257eca68ef>
2014-07-03 19:29:14 +0000

    chunks have shorts
    git-svn-id: svn://elaine/murooma/trunk@60 d8a302eb-03bc-478d-80e4-98257eca68ef

--- server.cpp
+++ server.cpp
@@ -42,7 +42,7 @@ int main () {
     {
 		c[0] = cin.get();
 		c[1] = cin.get();
-        chunk->payload[idx++] = (int)c[0] + ((int)c[1] * 256);
+        chunk->payload[idx++] = 0;//(int)c[0] + ((int)c[1] * 256);
         if (idx == WIRE_CHUNK_SIZE)
         {
             timeval now;
