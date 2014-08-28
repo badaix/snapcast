9c6ee38ce4aee40fa6f06b8afacaec4246a8810f
(no author) <(no author)@d8a302eb-03bc-478d-80e4-98257eca68ef>
2014-08-28 07:17:58 +0000

    portaudio
    git-svn-id: svn://elaine/murooma/trunk@217 d8a302eb-03bc-478d-80e4-98257eca68ef

--- server/pcmEncoder.cpp
+++ server/pcmEncoder.cpp
@@ -9,7 +9,7 @@ void PcmEncoder::encode(Chunk* chunk)
 {
 /*	WireChunk* wireChunk = chunk->wireChunk;
 	for (size_t n=0; n<wireChunk->length; ++n)
-		wireChunk->payload[n] *= 1.5;
+		wireChunk->payload[n] *= 1;
 */
 }
 
