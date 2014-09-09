ae79214e2ab1935075dbfb9785ffd88c01e34cb0
(no author) <(no author)@d8a302eb-03bc-478d-80e4-98257eca68ef>
2014-09-09 21:29:27 +0000

    logging
    git-svn-id: svn://elaine/murooma/trunk@248 d8a302eb-03bc-478d-80e4-98257eca68ef

--- client/serverConnection.cpp
+++ client/serverConnection.cpp
@@ -53,13 +53,10 @@ void ServerConnection::getNextMessage(tcp::socket* socket)
 {
 	BaseMessage baseMessage;
 	size_t baseMsgSize = baseMessage.getSize();
-cout << "1" << baseMsgSize << "\n";
 	vector<char> buffer(baseMsgSize);
-cout << "2\n";
 	socketRead(socket, &buffer[0], baseMsgSize);
-cout << "3\n";
 	baseMessage.deserialize(&buffer[0]);
-cout << "type: " << baseMessage.type << ", size: " << baseMessage.size << "\n";
+//cout << "type: " << baseMessage.type << ", size: " << baseMessage.size << "\n";
 	if (baseMessage.size > buffer.size())
 		buffer.resize(baseMessage.size);
 	socketRead(socket, &buffer[0], baseMessage.size);
