732fea38ae7900c52f17b65b2ab2e4b54b24e50c
(no author) <(no author)@d8a302eb-03bc-478d-80e4-98257eca68ef>
2014-09-24 20:05:55 +0000

    typedef
    git-svn-id: svn://elaine/murooma/trunk@291 d8a302eb-03bc-478d-80e4-98257eca68ef

--- common/timeDefs.h
+++ common/timeDefs.h
@@ -5,11 +5,11 @@
 
 namespace chronos
 {
-	using hrc = std::chrono::high_resolution_clock;
-	using time_point_hrc = std::chrono::time_point<hrc>;
-	using sec = std::chrono::seconds;
-	using msec = std::chrono::milliseconds;
-	using usec = std::chrono::microseconds;
+	typedef std::chrono::high_resolution_clock hrc;
+	typedef std::chrono::time_point<hrc> time_point_hrc;
+	typedef std::chrono::seconds sec;
+	typedef std::chrono::milliseconds msec;
+	typedef std::chrono::microseconds usec;
 }
 
 
