05c8107203c1cfe594cd68bd13e2d5f3e899e729
badaix <snapcast@badaix.de>
2015-09-11 21:02:13 +0200

    added control.py

--- control/setVolume.py
+++ control/setVolume.py
@@ -15,8 +15,8 @@ def doRequest( j, requestId ):
 		jResponse = json.loads(response)
 		if 'id' in jResponse:
 			if jResponse['id'] == requestId:
-				print("recv: " + response)
-				return;
+#				print("recv: " + response)
+				return jResponse;
 	return;
 
 def setVolume(client, volume):
@@ -25,11 +25,13 @@ def setVolume(client, volume):
 	requestId = requestId + 1
 
 volume = int(sys.argv[2])
-doRequest(json.dumps({'jsonrpc': '2.0', 'method': 'System.GetStatus', 'id': 1}), 1)
-setVolume("00:21:6a:7d:74:fc", volume)
-setVolume("80:1f:02:ed:fd:e0", volume)
-setVolume("74:da:38:00:85:e2", volume)
-setVolume("80:1f:02:ff:79:6e", volume)
-setVolume("bc:5f:f4:ca:cd:64", volume)
+j = doRequest(json.dumps({'jsonrpc': '2.0', 'method': 'System.GetStatus', 'id': 1}), 1)
+for client in j["result"]["clients"]:
+	setVolume(client['MAC'], volume)
+
+j = doRequest(json.dumps({'jsonrpc': '2.0', 'method': 'System.GetStatus', 'id': 1}), 1)
+for client in j["result"]["clients"]:
+	print("MAC: " + client['MAC'] + ", name: " + client['name'] + ", conntect: " + str(client['connected']) + ", volume: " + str(client['volume']['percent']))
+
 telnet.close
 
