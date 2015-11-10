b0652299bf997d6229bda132c3ce0d122be0ff2f
badaix <snapcast@badaix.de>
2015-11-10 20:08:59 +0100

    moved alsaPlayer into player subdir

---
+++ client/player/player.cpp
@@ -0,0 +1,74 @@
+/***
+    This file is part of snapcast
+    Copyright (C) 2015  Johannes Pohl
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+***/
+
+#include <iostream>
+
+#include "player.h"
+
+using namespace std;
+
+
+Player::Player(const PcmDevice& pcmDevice, Stream* stream) :
+	active_(false),
+	stream_(stream),
+	pcmDevice_(pcmDevice),
+	volume_(1.0),
+	muted_(false)
+{
+}
+
+
+
+void Player::start()
+{
+	active_ = true;
+	playerThread_ = thread(&Player::worker, this);
+}
+
+
+Player::~Player()
+{
+	stop();
+}
+
+
+
+void Player::stop()
+{
+	if (active_)
+	{
+		active_ = false;
+		playerThread_.join();
+	}
+}
+
+
+
+void Player::setVolume(double volume)
+{
+	volume_ = volume;
+}
+
+
+
+void Player::setMute(bool mute)
+{
+	muted_ = mute;
+}
+
+
