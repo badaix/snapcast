b5fc614f841e3e9b95444992bf7c4cbfc04ba4c7
badaix <snapcast@badaix.de>
2015-12-31 11:18:26 +0100

    dirty prototype

--- android/app/src/main/java/de/badaix/snapcast/MainActivity.java
+++ android/app/src/main/java/de/badaix/snapcast/MainActivity.java
@@ -1,22 +1,38 @@
 package de.badaix.snapcast;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.net.Uri;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.os.*;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.Process;
+import java.net.InetAddress;
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -31,6 +47,13 @@ public class MainActivity extends ActionBarActivity
      */
     private CharSequence mTitle;
 
+    private static final String TAG = "Main";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +67,64 @@ public class MainActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        copyAssets();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    private void copyAssets() {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+            Log.e("tag", "Failed to get asset file list.", e);
+        }
+        if (files != null) for (String filename : files) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                File outFile = new File(getFilesDir(), filename);
+//                if (outFile.exists() && (outFile.length() == assetManager.openFd(filename).getLength())) {
+//                    Log.d("Main", "Exists: " + outFile.getAbsolutePath());
+//                    continue;
+//                }
+                Log.d("Main", "Asset: " + outFile.getAbsolutePath());
+                in = assetManager.open(filename);
+                out = new FileOutputStream(outFile);
+                copyFile(in, out);
+                Runtime.getRuntime().exec("chmod 755 " + outFile.getAbsolutePath()).waitFor();
+            } catch (Exception e) {
+                Log.e("tag", "Failed to copy asset file: " + filename, e);
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        // NOOP
+                    }
+                }
+                if (out != null) {
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        // NOOP
+                    }
+                }
+            }
+        }
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
@@ -76,15 +156,68 @@ public class MainActivity extends ActionBarActivity
         actionBar.setTitle(mTitle);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://de.badaix.snapcast/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://de.badaix.snapcast/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private Button buttonScan;
+        private Button buttonStart;
+        private Button buttonStop;
+        private TextView tvHost;
+        private Process process = null;
+        private PowerManager.WakeLock wakeLock = null;
+        private Thread reader = null;
+        private NsdManager.DiscoveryListener mDiscoveryListener;
+        private NsdManager mNsdManager = null;
+        private String host = "";
+        private int port = 1704;
+
 
         public PlaceholderFragment() {
         }
@@ -105,6 +238,15 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            buttonScan = (Button) rootView.findViewById(R.id.buttonScan);
+            buttonStart = (Button) rootView.findViewById(R.id.buttonStart);
+            buttonStop = (Button) rootView.findViewById(R.id.buttonStop);
+            tvHost = (TextView) rootView.findViewById(R.id.tvHost);
+
+            buttonScan.setOnClickListener(this);
+            buttonStart.setOnClickListener(this);
+            buttonStop.setOnClickListener(this);
+            initializeDiscoveryListener();
             return rootView;
         }
 
@@ -113,6 +255,133 @@ public class MainActivity extends ActionBarActivity
             super.onAttach(activity);
             ((MainActivity) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+
+
+        private void start() {
+            try {
+                stop();
+                File binary = new File(this.getActivity().getFilesDir(), "snapclient");
+//                    process = Runtime.getRuntime().exec("\"" + binary.getAbsolutePath() + " -h elaine -p 33229\" 2>&1");
+                android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+                process = new ProcessBuilder()
+                        .command(binary.getAbsolutePath(), "-h", host, "-p", Integer.toString(port))
+                        .redirectErrorStream(true)
+                        .start();
+
+                PowerManager powerManager = (PowerManager) this.getActivity().getSystemService(POWER_SERVICE);
+                wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Snapcast");
+                wakeLock.acquire();
+
+                Thread reader = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        BufferedReader bufferedReader = new BufferedReader(
+                                new InputStreamReader(process.getInputStream()));
+                        String line;
+                        try {
+                            while ((line = bufferedReader.readLine()) != null) {
+                                Log.d("Main", line);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                reader.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void stop() {
+            if (process != null)
+                process.destroy();
+            if ((wakeLock != null) && wakeLock.isHeld())
+                wakeLock.release();
+            if (reader != null)
+                reader.interrupt();
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_DEFAULT);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (view == buttonStart) {
+                Toast.makeText(this.getActivity(), "Start", Toast.LENGTH_SHORT).show();
+                start();
+            } else if (view == buttonStop) {
+                Toast.makeText(this.getActivity(), "Stop", Toast.LENGTH_SHORT).show();
+                stop();
+            } else if (view == buttonScan) {
+                Toast.makeText(this.getActivity(), "Scan", Toast.LENGTH_SHORT).show();
+                initializeDiscoveryListener();
+            }
+        }
+
+        public void initializeDiscoveryListener() {
+
+            // Instantiate a new DiscoveryListener
+            mDiscoveryListener = new NsdManager.DiscoveryListener() {
+
+                //  Called as soon as service discovery begins.
+                @Override
+                public void onDiscoveryStarted(String regType) {
+                    Log.d(TAG, "Service discovery started");
+                }
+
+                @Override
+                public void onServiceFound(NsdServiceInfo service) {
+                    // A service was found!  Do something with it.
+                    Log.d(TAG, "Service discovery success" + service);
+                    mNsdManager.resolveService(service, new NsdManager.ResolveListener() {
+                        @Override
+                        public void onResolveFailed(NsdServiceInfo nsdServiceInfo, int i) {
+
+                        }
+
+                        @Override
+                        public void onServiceResolved(NsdServiceInfo nsdServiceInfo) {
+                            Log.d(TAG, "resolved: " + nsdServiceInfo);
+                            host = nsdServiceInfo.getHost().getCanonicalHostName();
+                            port = nsdServiceInfo.getPort();
+                            tvHost.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvHost.setText("Host: " + host + ":" + port);
+                                }
+                            });
+                            mNsdManager.stopServiceDiscovery(mDiscoveryListener);
+                        }
+                    });
+                }
+
+                @Override
+                public void onServiceLost(NsdServiceInfo service) {
+                    // When the network service is no longer available.
+                    // Internal bookkeeping code goes here.
+                    Log.e(TAG, "service lost" + service);
+                }
+
+                @Override
+                public void onDiscoveryStopped(String serviceType) {
+                    Log.i(TAG, "Discovery stopped: " + serviceType);
+                }
+
+                @Override
+                public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+                    Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+                    mNsdManager.stopServiceDiscovery(this);
+                }
+
+                @Override
+                public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+                    Log.e(TAG, "Discovery failed: Error code:" + errorCode);
+                    mNsdManager.stopServiceDiscovery(this);
+                }
+            };
+
+            mNsdManager = (NsdManager) this.getActivity().getSystemService(Context.NSD_SERVICE);
+            mNsdManager.discoverServices("_snapcast._tcp.", NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
         }
     }
 
