c61f9a8c28427696ad569c65cde5e289f6bcabfc
badaix <snapcast@badaix.de>
2016-01-19 19:42:17 +0100

    Filled About screen with content

--- android/Snapcast/src/main/java/de/badaix/snapcast/AboutActivity.java
+++ android/Snapcast/src/main/java/de/badaix/snapcast/AboutActivity.java
@@ -2,6 +2,7 @@ package de.badaix.snapcast;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        WebView wv= (WebView)findViewById(R.id.webView);
+        wv.loadUrl("file:///android_asset/" + this.getText(R.string.about_file));
     }
 }
