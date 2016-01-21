fe89220b15763b03245a0c3735063df0f711b7bb
badaix <snapcast@badaix.de>
2016-01-21 22:02:09 +0100

    2016

--- server/pcmreader/pcmReader.cpp
+++ server/pcmreader/pcmReader.cpp
@@ -1,6 +1,6 @@
 /***
     This file is part of snapcast
-    Copyright (C) 2015  Johannes Pohl
+    Copyright (C) 2014-2016  Johannes Pohl
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
