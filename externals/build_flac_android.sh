5f733697c7e65d0a065b056b5f94773a97437219
badaix <snapcast@badaix.de>
2016-03-13 18:57:16 +0100

    NDK_DIR

--- externals/build_flac_android.sh
+++ externals/build_flac_android.sh
@@ -1,5 +1,9 @@
-export CC=/home/johannes/Develop/android-ndk-r10e-arm-21/bin/arm-linux-androideabi-gcc
-export CXX=/home/johannes/Develop/android-ndk-r10e-arm-21/bin/arm-linux-androideabi-g++
+export NDK_DIR="/home/johannes/Develop/android-toolchain-arm-14"
+export CC=$NDK_DIR/bin/arm-linux-androideabi-gcc
+export CXX=$NDK_DIR/bin/arm-linux-androideabi-g++
 #export INC=$(SYSROOT)/usr/include
 cd flac
+./autogen.sh
 ./configure --host=arm --disable-ogg --prefix=/home/johannes/Develop/build/arm
+make
+make install
