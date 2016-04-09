1bc384916b7f499fb9d7ce3b2b359aac7aa70aec
badaix <snapcast@badaix.de>
2016-04-09 13:55:12 +0200

    install FLAC in the NDK dir

--- externals/build_flac_android.sh
+++ externals/build_flac_android.sh
@@ -1,9 +1,8 @@
 export NDK_DIR="/home/johannes/Develop/android-toolchain-arm-14"
 export CC=$NDK_DIR/bin/arm-linux-androideabi-gcc
 export CXX=$NDK_DIR/bin/arm-linux-androideabi-g++
-#export INC=$(SYSROOT)/usr/include
 cd flac
 ./autogen.sh
-./configure --host=arm --disable-ogg --prefix=/home/johannes/Develop/build/arm
+./configure --host=arm --disable-ogg --prefix=$NDK_DIR
 make
 make install
