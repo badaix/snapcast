94c139710c0b393bcb0997ea8a95e74056ac41d7
Joseph D <joseph@outurnate.com>
2016-08-01 12:50:24 -0400

    windows script update

--- externals/windows.sh
+++ externals/windows.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+mkdir -p target/lib/pkgconfig
+pushd target
+TARGET_DIR=`pwd`
+pushd lib/pkgconfig
+PKG_DIR=`pwd`
+popd
+popd
+
+export PKG_CONFIG_PATH=$PKG_DIR
+
 mkdir -p ogg_build
 pushd ogg_build
-cmake ../ogg -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=../target -G "MSYS Makefiles"
+cmake ../ogg -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=$TARGET_DIR -G "MSYS Makefiles"
 cmake --build . --target install
 popd
 
 pushd flac
+dos2unix configure.ac
 ./autogen.sh
-./configure --prefix ../target
+./configure --prefix $TARGET_DIR
 make install
 popd
 
 mkdir -p vorbis_build
 pushd vorbis
-cmake ../vorbis -DCMAKE_INSTALL_PREFIX=../target -G "MSYS Makefiles"
+cmake ../vorbis -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=$TARGET_DIR -G "MSYS Makefiles"
 cmake --build . --target install
 popd
