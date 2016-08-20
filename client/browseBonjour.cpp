9baa74d115f270dc70ec6bcda1ca8c821b1bda9d
Joseph D <joseph@outurnate.com>
2016-08-20 15:40:28 -0400

    bonjour - working minus timeout

--- client/browseBonjour.cpp
+++ client/browseBonjour.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 #include <deque>
 #include <sys/socket.h>
+#include <netdb.h>
 
+#include "common/log.h"
 #include "common/snapException.h"
 
 using namespace std;
@@ -89,6 +91,33 @@ string BonjourGetError(DNSServiceErrorType error)
 		
 	case kDNSServiceErr_BadTime:
 		return "BadTime";
+		
+	case kDNSServiceErr_BadSig:
+		return "BadSig";
+		
+	case kDNSServiceErr_BadKey:
+		return "BadKey";
+		
+	case kDNSServiceErr_Transient:
+		return "Transient";
+		
+	case kDNSServiceErr_ServiceNotRunning:
+		return "ServiceNotRunning";
+		
+	case kDNSServiceErr_NATPortMappingUnsupported:
+		return "NATPortMappingUnsupported";
+		
+	case kDNSServiceErr_NATPortMappingDisabled:
+		return "NATPortMappingDisabled";
+		
+	case kDNSServiceErr_NoRouter:
+		return "NoRouter";
+		
+	case kDNSServiceErr_PollingMode:
+		return "PollingMode";
+		
+	case kDNSServiceErr_Timeout:
+		return "Timeout";
 	}
 }
 
@@ -103,39 +132,7 @@ struct mDNSResolve
 	uint16_t port;
 };
 
-#define CHECKED(err) if((err)!=kDNSServiceErr_NoError)throw SnapException(BonjourGetError(err));
-
-void BrowseBonjour::BonjourResolve(DNSServiceRef service,
-																	 DNSServiceFlags flags,
-																	 uint32_t interfaceIndex,
-																	 DNSServiceErrorType errorCode,
-																	 const char* fullName,
-																	 const char* hosttarget,
-																	 uint16_t port,
-																	 uint16_t txtLen,
-																	 const unsigned char* txtRecord,
-																	 void* context)
-{
-	auto resultCollection = static_cast<deque<mDNSResolve>*>(context);
-
-	CHECKED(errorCode);
-	resultCollection->push_back(mDNSResolve { string(fullName), port });
-}
-
-void BrowseBonjour::BonjourReply(DNSServiceRef service,
-																 DNSServiceFlags flags,
-																 uint32_t interfaceIndex,
-																 DNSServiceErrorType errorCode,
-																 const char* serviceName,
-																 const char* regtype,
-																 const char* replyDomain,
-																 void* context)
-{
-	auto replyCollection = static_cast<deque<mDNSReply>*>(context);
-	
-	CHECKED(errorCode);
-	replyCollection->push_back(mDNSReply { string(serviceName), string(regtype), string(replyDomain) });
-}
+#define CHECKED(err) if((err)!=kDNSServiceErr_NoError)throw SnapException(BonjourGetError(err) + to_string(__LINE__));
 
 void runService(const DNSServiceRef& service, timeval& timeout)
 {
@@ -160,7 +157,21 @@ bool BrowseBonjour::browse(const string& serviceName, mDNSResult& result, int ti
 	deque<mDNSReply> replyCollection;
 	{
 		DNSServiceRef service = NULL;
-		CHECKED(DNSServiceBrowse(&service, 0, 0, "_snapcast._tcp", "local.", BrowseBonjour::BonjourReply, &replyCollection));
+		CHECKED(DNSServiceBrowse(&service, 0, 0, serviceName.c_str(), "local.",
+														 [](DNSServiceRef service,
+																DNSServiceFlags flags,
+																uint32_t interfaceIndex,
+																DNSServiceErrorType errorCode,
+																const char* serviceName,
+																const char* regtype,
+																const char* replyDomain,
+																void* context)
+														 {
+															 auto replyCollection = static_cast<deque<mDNSReply>*>(context);
+															 
+															 CHECKED(errorCode);
+															 replyCollection->push_back(mDNSReply{ string(serviceName), string(regtype), string(replyDomain) });
+														 }, &replyCollection));
 
 		runService(service, timeoutVal);
 		
@@ -170,36 +181,71 @@ bool BrowseBonjour::browse(const string& serviceName, mDNSResult& result, int ti
 	timeoutVal.tv_sec = 5;
 	timeoutVal.tv_usec = 0;
 
-	// Results
+	// Resolve
 	deque<mDNSResolve> resolveCollection;
 	{
 		DNSServiceRef service = NULL;
 		for (auto& reply : replyCollection)
-			CHECKED(DNSServiceResolve(&service, 0, 0, reply.name.c_str(), reply.regtype.c_str(), reply.domain.c_str(), BrowseBonjour::BonjourResolve, &resolveCollection));
+			CHECKED(DNSServiceResolve(&service, 0, 0, reply.name.c_str(), reply.regtype.c_str(), reply.domain.c_str(),
+																[](DNSServiceRef service,
+																	 DNSServiceFlags flags,
+																	 uint32_t interfaceIndex,
+																	 DNSServiceErrorType errorCode,
+																	 const char* fullName,
+																	 const char* hosttarget,
+																	 uint16_t port,
+																	 uint16_t txtLen,
+																	 const unsigned char* txtRecord,
+																	 void* context)
+																{
+																	auto resultCollection = static_cast<deque<mDNSResolve>*>(context);
+																	
+																	CHECKED(errorCode);
+																	resultCollection->push_back(mDNSResolve { string(hosttarget), ntohs(port) });
+																}, &resolveCollection));
 
 		runService(service, timeoutVal);
 		
 		DNSServiceRefDeallocate(service); // TODO wrap RAII
 	}
+	
+	timeoutVal.tv_sec = 5;
+	timeoutVal.tv_usec = 0;
 
 	// DNS/mDNS Resolve
-	deque<mDNSResult> resultCollection;
+	deque<mDNSResult> resultCollection(resolveCollection.size(), mDNSResult { 0, "", "", 0, false });
 	{
 		DNSServiceRef service = NULL;
+		unsigned i = 0;
 		for (auto& resolve : resolveCollection)
 		{
-			auto port = resolve.port;
-			CHECKED(DNSServiceGetAddrInfo(&service, 0, 0, 0, resolve.fullName,
-																		[port](DNSServiceRef service,
-																					 DNSServiceFlags flags,
-																					 uint32_t interface_index,
-																					 DNSServiceErrorType errorCode,
-																					 const char* hostname,
-																					 const sockaddr* address,
-																					 uint32_t ttl,
-																					 void* context)
+			resultCollection[i].port_ = resolve.port;
+			CHECKED(DNSServiceGetAddrInfo(&service, kDNSServiceFlagsLongLivedQuery, 0, kDNSServiceProtocol_IPv4, resolve.fullName.c_str(),
+																		[](DNSServiceRef service,
+																			 DNSServiceFlags flags,
+																			 uint32_t interfaceIndex,
+																			 DNSServiceErrorType errorCode,
+																			 const char* hostname,
+																			 const sockaddr* address,
+																			 uint32_t ttl,
+																			 void* context)
 																		{
-																		}, &resultCollection));
+																			auto result = static_cast<mDNSResult*>(context);
+
+																			result->host_ = string(hostname);
+																			result->proto_ = address->sa_family;
+
+																			char hostIP[NI_MAXHOST];
+																			char hostService[NI_MAXSERV];
+																			if (getnameinfo(address, sizeof(*address),
+																											hostIP, sizeof(hostIP),
+																											hostService, sizeof(hostService),
+																											NI_NUMERICHOST|NI_NUMERICSERV) == 0)
+																				result->ip_ = string(hostIP);
+																			else
+																				return;
+																			result->valid_ = true;
+																		}, &resultCollection[i++]));
 		}
 		
 		runService(service, timeoutVal);
@@ -207,10 +253,13 @@ bool BrowseBonjour::browse(const string& serviceName, mDNSResult& result, int ti
 		DNSServiceRefDeallocate(service); // TODO wrap RAII
 	}
 
-	cout << replyCollection.size() << endl;
-	cout << resultCollection.size() << endl;
-	for (auto& result : resultCollection)
-		cout << result.host_ << endl;
+	if (resultCollection.size() == 0)
+		return false;
+
+	if (resultCollection.size() != 1)
+		logO << "Multiple servers found.  Using first" << endl;
+
+	result = resultCollection[0];
 	
 	return true;
 }
