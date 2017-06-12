d0cbf06fd6ee9b624b6fcaa52f2b89b52d3666f4
badaix <johannes.pohl@badaix.de>
2017-06-12 08:12:11 +0200

    check NDK environment variables

--- client/build_android.sh
+++ client/build_android.sh
@@ -1,9 +1,28 @@
-export NDK_DIR="/home/johannes/Develop/android-toolchain-arm"
-export ARCH=arm
-make clean; make TARGET=ANDROID -j 4; cp ./snapclient ../android/Snapcast/src/main/assets/bin/armeabi/
-export NDK_DIR="/home/johannes/Develop/android-toolchain-mips"
-export ARCH=mips
-make clean; make TARGET=ANDROID -j 4; cp ./snapclient ../android/Snapcast/src/main/assets/bin/mips/
-export NDK_DIR="/home/johannes/Develop/android-toolchain-x86"
-export ARCH=x86
-make clean; make TARGET=ANDROID -j 4; cp ./snapclient ../android/Snapcast/src/main/assets/bin/x86/
+#/bin/sh
+
+export NDK_DIR_ARM="/home/johannes/Develop/android-toolchain-arm"
+export NDK_DIR_MIPS="/home/johannes/Develop/android-toolchain-mips"
+export NDK_DIR_X86="/home/johannes/Develop/android-toolchain-x86"
+
+if [ -z "$NDK_DIR_ARM" ] && [ -z "$NDK_DIR_MIPS" ] && [ -z "$NDK_DIR_X86" ]; then
+	echo "Specify at least one NDK_DIR_[ARM|MIPS|X86]"
+	exit
+fi
+
+if [ -n "$NDK_DIR_ARM" ]; then
+	export NDK_DIR="$NDK_DIR_ARM"
+	export ARCH=arm
+	make clean; make TARGET=ANDROID -j 4; mv ./snapclient ../android/Snapcast/src/main/assets/bin/armeabi/
+fi
+
+if [ -n "$NDK_DIR_MIPS" ]; then
+	export NDK_DIR="$NDK_DIR_MIPS"
+	export ARCH=mips
+	make clean; make TARGET=ANDROID -j 4; mv ./snapclient ../android/Snapcast/src/main/assets/bin/mips/
+fi
+
+if [ -n "$NDK_DIR_X86" ]; then
+	export NDK_DIR="$NDK_DIR_X86"
+	export ARCH=x86
+	make clean; make TARGET=ANDROID -j 4; mv ./snapclient ../android/Snapcast/src/main/assets/bin/x86/
+fi
