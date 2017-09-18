3ab268f5acb30c3bcb1651083d1227fb09344f2b
Mike Walters <mike@flomp.net>
2017-09-18 22:17:51 +0100

    Support connecting to an IPv6 host in snapclient

--- client/clientConnection.cpp
+++ client/clientConnection.cpp
@@ -70,7 +70,7 @@ std::string ClientConnection::getMacAddress() const
 void ClientConnection::start()
 {
 	tcp::resolver resolver(io_service_);
-	tcp::resolver::query query(tcp::v4(), host_, cpt::to_string(port_), asio::ip::resolver_query_base::numeric_service);
+	tcp::resolver::query query(host_, cpt::to_string(port_), asio::ip::resolver_query_base::numeric_service);
 	auto iterator = resolver.resolve(query);
 	logO << "Connecting\n";
 	socket_.reset(new tcp::socket(io_service_));
