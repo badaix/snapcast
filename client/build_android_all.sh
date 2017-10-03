9c97d0576e59ba07f6c7ed925fb56caff5b9dbba
badaix <johannes.pohl@badaix.de>
2017-10-03 16:14:47 +0200

    fix building for Android

--- client/build_android_all.sh
+++ client/build_android_all.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-export NDK_DIR_ARM="/home/johannes/Develop/android-toolchain-arm"
-export NDK_DIR_MIPS="/home/johannes/Develop/android-toolchain-mips"
-export NDK_DIR_X86="/home/johannes/Develop/android-toolchain-x86"
+export NDK_DIR_ARM="$1-arm"
+export NDK_DIR_MIPS="$1-mips"
+export NDK_DIR_X86="$1-x86"
 
 ./build_android.sh
