44046821c1b2c73f1189a04b08a3b40f7528eeef
badaix <johannes.pohl@badaix.de>
2018-02-19 20:04:28 +0100

    update date in copyright

--- client/snapClient.cpp
+++ client/snapClient.cpp
@@ -1,6 +1,6 @@
 /***
     This file is part of snapcast
-    Copyright (C) 2014-2017  Johannes Pohl
+    Copyright (C) 2014-2018  Johannes Pohl
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -116,7 +116,7 @@ int main (int argc, char **argv)
 		if (versionSwitch->is_set())
 		{
 			cout << "snapclient v" << VERSION << "\n"
-				<< "Copyright (C) 2014-2017 BadAix (snapcast@badaix.de).\n"
+				<< "Copyright (C) 2014-2018 BadAix (snapcast@badaix.de).\n"
 				<< "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
 				<< "This is free software: you are free to change and redistribute it.\n"
 				<< "There is NO WARRANTY, to the extent permitted by law.\n\n"
--- server/streamreader/process.hpp
+++ server/streamreader/process.hpp
@@ -1,6 +1,6 @@
 /***
     This file is part of snapcast
-    Copyright (C) 2014-2017  Johannes Pohl
+    Copyright (C) 2014-2018  Johannes Pohl
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
