6d490670ca60df0b47b37d536d1233ce439d6def
badaix <johannes.pohl@badaix.de>
2018-03-13 07:50:02 +0100

    fix crash on pre 5.0 Android

--- android/Snapcast/src/main/java/de/badaix/snapcast/ServerDialogFragment.java
+++ android/Snapcast/src/main/java/de/badaix/snapcast/ServerDialogFragment.java
@@ -23,7 +23,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.net.nsd.NsdServiceInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
