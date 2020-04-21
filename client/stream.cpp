4501bbe2063e7093927845765ee5e9a76ae4deda
badaix <johannes.pohl@badaix.de>
2020-04-21 11:01:07 +0200

    Speed up time sync after standby

--- client/stream.cpp
+++ client/stream.cpp
@@ -16,7 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ***/
 
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif // NOMINMAX
 
 #include "stream.hpp"
 #include "common/aixlog.hpp"
