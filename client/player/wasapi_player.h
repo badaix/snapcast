7fd7c49538998906535fc0c446f6e04ad0a0cb0d
Stijn Van der Borght <stijnvanderborght@gmail.com>
2020-05-10 13:26:32 +0100

    wasapi: added listener for getting system volume

--- client/player/wasapi_player.h
+++ client/player/wasapi_player.h
@@ -21,12 +21,14 @@
 #include "client_settings.hpp"
 #include "player.hpp"
 #include <audiopolicy.h>
+#include <endpointvolume.h>
 
 class AudioSessionEventListener : public IAudioSessionEvents
 {
     LONG _cRef;
 
     float volume_ = 1.f;
+    bool muted_ = false;
 
 public:
     AudioSessionEventListener() : _cRef(1)
@@ -36,6 +38,11 @@ public:
     float getVolume()
     {
         return volume_;
+    }
+
+    bool getMuted()
+    {
+        return muted_;
     }
 
     ~AudioSessionEventListener()
@@ -90,6 +97,75 @@ public:
     HRESULT STDMETHODCALLTYPE OnSessionDisconnected(AudioSessionDisconnectReason DisconnectReason);
 };
 
+
+class AudioEndpointVolumeCallback : public IAudioEndpointVolumeCallback
+{
+    LONG _cRef;
+    float volume_ = 1.f;
+    bool muted_ = false;
+
+public:
+    AudioEndpointVolumeCallback() : _cRef(1)
+    {
+    }
+
+    ~AudioEndpointVolumeCallback()
+    {
+    }
+
+    float getVolume()
+    {
+        return volume_;
+    }
+
+    bool getMuted()
+    {
+        return muted_;
+    }
+
+    // IUnknown methods -- AddRef, Release, and QueryInterface
+
+    ULONG STDMETHODCALLTYPE AddRef()
+    {
+        return InterlockedIncrement(&_cRef);
+    }
+
+    ULONG STDMETHODCALLTYPE Release()
+    {
+        ULONG ulRef = InterlockedDecrement(&_cRef);
+        if (0 == ulRef)
+        {
+            delete this;
+        }
+        return ulRef;
+    }
+
+    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, VOID** ppvInterface)
+    {
+        if (IID_IUnknown == riid)
+        {
+            AddRef();
+            *ppvInterface = (IUnknown*)this;
+        }
+        else if (__uuidof(IAudioEndpointVolumeCallback) == riid)
+        {
+            AddRef();
+            *ppvInterface = (IAudioEndpointVolumeCallback*)this;
+        }
+        else
+        {
+            *ppvInterface = NULL;
+            return E_NOINTERFACE;
+        }
+
+        return S_OK;
+    }
+
+    // Callback method for endpoint-volume-change notifications.
+
+    HRESULT STDMETHODCALLTYPE OnNotify(PAUDIO_VOLUME_NOTIFICATION_DATA pNotify);
+};
+
 class WASAPIPlayer : public Player
 {
 public:
@@ -103,6 +179,7 @@ protected:
 
 private:
     AudioSessionEventListener* audioEventListener_;
+    IAudioEndpointVolume* audioEndpointListener_;
     ClientSettings::SharingMode mode_;
 };
 
