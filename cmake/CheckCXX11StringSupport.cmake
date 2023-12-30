6441ca0a99200ea88c415a744801a5d4791dc2de
badaix <johannes.pohl@badaix.de>
2023-12-30 11:37:50 +0100

    Reformat code

--- cmake/CheckCXX11StringSupport.cmake
+++ cmake/CheckCXX11StringSupport.cmake
@@ -1,18 +1,17 @@
-# This file is part of snapcast
-# Copyright (C) 2014-2020  Johannes Pohl
+# This file is part of snapcast Copyright (C) 2014-2020  Johannes Pohl
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #[=======================================================================[.rst:
 CheckCXX11StringSupport
@@ -33,12 +32,12 @@ See also: https://stackoverflow.com/questions/17950814/how-to-use-stdstoul-and-s
   failure.
 #]=======================================================================]
 
-
 include(CheckCXXSourceCompiles)
 
-macro (CHECK_CXX11_STRING_SUPPORT _RESULT)
-    set(CMAKE_REQUIRED_DEFINITIONS -std=c++0x)
-    set(_CHECK_CXX11_STRING_SUPPORT_SOURCE_CODE "
+macro(CHECK_CXX11_STRING_SUPPORT _RESULT)
+  set(CMAKE_REQUIRED_DEFINITIONS -std=c++0x)
+  set(_CHECK_CXX11_STRING_SUPPORT_SOURCE_CODE
+      "
 #include <string>
 int main()
 {
@@ -50,5 +49,6 @@ int main()
 }
 ")
 
-    CHECK_CXX_SOURCE_COMPILES("${_CHECK_CXX11_STRING_SUPPORT_SOURCE_CODE}" ${_RESULT})
-endmacro ()
+  check_cxx_source_compiles("${_CHECK_CXX11_STRING_SUPPORT_SOURCE_CODE}"
+                            ${_RESULT})
+endmacro()
