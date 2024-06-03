546572d6a026450433bf9c3e8bd1a1db05dc2cc0
badaix <johannes.pohl@badaix.de>
2024-06-03 22:21:01 +0200

    JWT improvements

--- common/jwt.cpp
+++ common/jwt.cpp
@@ -23,6 +23,11 @@
 #include "common/aixlog.hpp"
 #include "common/base64.h"
 #include "common/utils/string_utils.hpp"
+#include <chrono>
+#include <cstdint>
+#include <exception>
+#include <openssl/x509.h>
+#include <optional>
 #include <sys/types.h>
 
 // 3rd party headers
@@ -45,7 +50,7 @@ static constexpr auto LOG_TAG = "JWT";
 namespace
 {
 
-EVP_PKEY* createPrivate(const std::string& key)
+EVP_PKEY* readKey(const std::string& key)
 {
     // Reads PEM information and retrieves some details
     BIO* keybio = BIO_new_mem_buf((void*)key.c_str(), -1);
@@ -83,120 +88,230 @@ EVP_PKEY* createPrivate(const std::string& key)
 }
 
 
-bool Sign(const std::string& pem_key, const std::string& msg, std::vector<unsigned char>& encoded)
+EVP_PKEY* readCert(const std::string& key)
 {
-    auto* key = createPrivate(pem_key);
-    EVP_MD_CTX* ctx = EVP_MD_CTX_create();
-    if (EVP_DigestSignInit(ctx, nullptr, EVP_sha384(), nullptr, key) <= 0)
+    // Reads PEM information and retrieves some details
+    BIO* keybio = BIO_new_mem_buf((void*)key.c_str(), -1);
+    if (keybio == nullptr)
+    {
+        LOG(ERROR, LOG_TAG) << "BIO_new_mem_buf failed\n";
+        return nullptr;
+    }
+
+    char* name = nullptr;
+    char* header = nullptr;
+    uint8_t* data = nullptr;
+    long datalen = 0;
+    if (PEM_read_bio(keybio, &name, &header, &data, &datalen) == 1)
+    {
+        // Copies the data pointer. D2I functions update it
+        const auto* data_pkey = reinterpret_cast<const uint8_t*>(data);
+        // Detects type and decodes the private key
+        X509* x509 = d2i_X509(nullptr, &data_pkey, datalen);
+        EVP_PKEY* pkey = X509_get_pubkey(x509);
+        if (pkey == nullptr)
+        {
+            LOG(ERROR, LOG_TAG) << "d2i_AutoPrivateKey failed\n";
+        }
+
+        // Free is only required after a PEM_bio_read successful return
+        if (name != nullptr)
+            OPENSSL_free(name);
+        if (header != nullptr)
+            OPENSSL_free(header);
+        if (data != nullptr)
+            OPENSSL_free(data);
+        return pkey;
+    }
+    return nullptr;
+}
+
+bool sign(const std::string& pem_key, const std::string& msg, std::vector<unsigned char>& encoded)
+{
+    auto* key = readKey(pem_key);
+    std::shared_ptr<EVP_MD_CTX> ctx(EVP_MD_CTX_create(), [](auto p) { EVP_MD_CTX_free(p); });
+
+    if (EVP_DigestSignInit(ctx.get(), nullptr, EVP_sha256(), nullptr, key) <= 0)
     {
         LOG(ERROR, LOG_TAG) << "EVP_DigestSignInit failed\n";
         return false;
     }
-    if (EVP_DigestSignUpdate(ctx, msg.c_str(), msg.size()) <= 0)
+    if (EVP_DigestSignUpdate(ctx.get(), msg.c_str(), msg.size()) <= 0)
     {
         LOG(ERROR, LOG_TAG) << "EVP_DigestSignUpdate failed\n";
         return false;
     }
     size_t siglen;
-    if (EVP_DigestSignFinal(ctx, nullptr, &siglen) <= 0)
+    if (EVP_DigestSignFinal(ctx.get(), nullptr, &siglen) <= 0)
     {
         LOG(ERROR, LOG_TAG) << "EVP_DigestSignFinal failed\n";
         return false;
     }
 
     encoded.resize(siglen);
-    if (EVP_DigestSignFinal(ctx, encoded.data(), &siglen) <= 0)
+    if (EVP_DigestSignFinal(ctx.get(), encoded.data(), &siglen) <= 0)
     {
         LOG(ERROR, LOG_TAG) << "EVP_DigestSignFinal failed\n";
         return false;
     }
-    EVP_MD_CTX_free(ctx);
     return true;
+}
+
+
+bool verifySignature(const std::string& pem_cert, unsigned char* MsgHash, size_t MsgHashLen, const char* Msg, size_t MsgLen, bool& Authentic)
+{
+    Authentic = false;
+    auto* key = readCert(pem_cert);
+    EVP_MD_CTX* ctx = EVP_MD_CTX_create();
+
+    if (EVP_DigestVerifyInit(ctx, nullptr, EVP_sha256(), nullptr, key) <= 0)
+    {
+        LOG(ERROR, LOG_TAG) << "EVP_DigestVerifyInit failed\n";
+        return false;
+    }
+    if (EVP_DigestVerifyUpdate(ctx, Msg, MsgLen) <= 0)
+    {
+        LOG(ERROR, LOG_TAG) << "EVP_DigestVerifyInit failed\n";
+        return false;
+    }
+    int authStatus = EVP_DigestVerifyFinal(ctx, MsgHash, MsgHashLen);
+    EVP_MD_CTX_free(ctx);
+    if (authStatus == 1)
+    {
+        Authentic = true;
+        return true;
+    }
+    if (authStatus == 0)
+    {
+        Authentic = false;
+        return true;
+    }
+    LOG(ERROR, LOG_TAG) << "EVP_DigestVerifyFinal failed: " << authStatus << "\n";
+    return false;
 }
 
 } // namespace
 
-Jwt::Jwt()
+Jwt::Jwt() : claims({})
 {
-    // {
-    //   "alg": "RS256",
-    //   "typ": "JWT"
-    // }
-    json header = {{"alg", "RS384"}, {"typ", "JWT"}};
-
-    // {
-    //   "sub": "1234567890",
-    //   "name": "Johannes",
-    //   "admin": true,
-    //   "iat": 1516239022
-    // }
-    json payload = {{"sub", "1234567890"}, {"name", "Johannes"}, {"admin", true}, {"iat", 1516239022}};
-
-    LOG(INFO, LOG_TAG) << "Header: " << header << "\n";
-    LOG(INFO, LOG_TAG) << "Payload: " << payload << "\n";
-    std::string msg = base64url_encode(header.dump()) + "." + base64url_encode(payload.dump());
-    LOG(INFO, LOG_TAG) << "Encoded: " << msg << "\n";
-
-    auto key = "-----BEGIN PRIVATE KEY-----\n"
-               "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCwFxHHKvV6THj1\n"
-               "VjvZQJIW+OjIKF9MPfIN8wJTXn+4EkBJCoBy0NfKHG+FCb90YCPzvLrjL8jKcfhT\n"
-               "/jpaStrYYjABXA1sJS9P+9cwiV9RwTxTrfOiss8F7eZdyZI9UogrPmQa7YbevmwB\n"
-               "XeXIqKzdWQbtQsOJQgoL3vIR7qjUpVQrXPwAYQ6pc7AxS5RHFy8V2Y3sh/+i0aS9\n"
-               "bH/276OAKZNwVIfUIcSVVbBAPvrheR2ezUVoKSumUzek/2uq3cLb5YNF4XSe1Ikv\n"
-               "16lRSGTIQ2KflSYn3mJldFjEKL3sgADTmMhKes+TVveNr7eCvynyDCtHdiLanOKY\n"
-               "PyyOXFTdAgMBAAECggEAAjunU6UBZsBhrUzKEVZ5XnYKxP+xZlmyat0Iy8QFmcY4\n"
-               "z076iNo0o6u/efVWb/RIfcQILlmHXJKG7BEWg4Qc/oRPkwjW47xHULvtw0AOt85G\n"
-               "mfy5UPgfBMvQRs1c/87piqYt0KNFTqhQCCa9GKcTmsf7p5ZtPTLw8Sxt2e6H8LsK\n"
-               "60Jzc2Yw2t3unEb1NnjsTgshjPwFcdrppyRAa2B0Wk3f4ADA1i4vDmTt2+jTq/Hp\n"
-               "yFWup58Djs+lyn4RLnp7jFD2KS/q+qVQsTfGcPeXLMIWHHQDwfjfkzdA74zNi+Pn\n"
-               "C4e/iXIsC7VH4BJ8qrVH20WqTXRyuZ1uEF+32XbcSQKBgQDAtBXbnuGhu2llnfP2\n"
-               "dxJ5qtRjxTHedZs9UMEuy5pkLMi4JsIdjf072lqizpG7DE9kfiERwXJ/Spe4OMMh\n"
-               "MvWBpnJieTHnouAMpDVootVbSCpikOSGzClHZwpl6KU7pv9Q+Hv2xkSnTJLsakSt\n"
-               "qlOsG6cwK56kXiwYG0RsAn6lhQKBgQDp7gNE4J6wf9SZHErpyh+65VqqcF5+chNq\n"
-               "DTwFlb7U31WcDOA3hUHaQfrlwfblMAgnlVMofopdP4KIIgSWE31cCziBp8ZRy/25\n"
-               "2/aNkDoPEN59Ibk1RWLYsCzcQIAQrTjvfDMn3An1E9B3qYFzfdLKZItb8p3cxpO/\n"
-               "sMUwQRqFeQKBgDb7av0lwQUPXwwiXDhnUvsp9b2dxxPNBIUjJGuApkWMzZxVWq9q\n"
-               "EuXf8FphjA0Nfx2SK0dQpaWSF+X1NB+l1YyvfBWCtO19eGXC+IYpZ6zK02UaKEoZ\n"
-               "uHFqAfp/vZ1ekZx9uYj4myAM5iLUU1Iltgf2P+arm3EUeYpLRWN39sCtAoGBALZ0\n"
-               "egBC4gLv8TXqp1Np3w26zdiaBFnDR/kzkVkZztnhx7gLIuaq/Q3q4HJLsvJXYETf\n"
-               "ZxjyeaD5ZCohvkn/sYsVBWG7JieuX5uTQN5xW5dcpOwcXYR7Nfmkj5jKhhh7wyin\n"
-               "So8QRIPujG6IuvsFbF+HxFpXBWGpUJv2mBZm8PShAoGBALU/KNl1El0HpxTr3wgj\n"
-               "YY6eU3snn0Oa5Ci3k/FMWd1QTtVsf4Mym0demxshdC75L+qzCS0m5jAo9tm0keY9\n"
-               "A4F3VRlsuUyuAja+qDU2xMo3jnFKOIyMfN4mVSiFkqnq3eQ4xHgViyIEyr+8AbA4\n"
-               "ajjiCZsv+OITxQ+TTHeGDsdD\n"
-               "-----END PRIVATE KEY-----\n";
-
-    std::vector<unsigned char> encoded;
-    if (Sign(key, msg, encoded))
-    {
-        std::string signature = base64url_encode(encoded.data(), encoded.size());
-        LOG(INFO, LOG_TAG) << "Signature: " << signature << "\n";
-    }
-    else
-    {
-        LOG(ERROR, LOG_TAG) << "Failed to sign\n";
-    }
 }
 
+std::optional<std::chrono::seconds> Jwt::getIat() const
+{
+    if (!claims.contains("iat"))
+        return std::nullopt;
+    return std::chrono::seconds(claims.at("iat").get<int64_t>());
+}
 
-bool Jwt::decode(const std::string& token)
+void Jwt::setIat(const std::optional<std::chrono::seconds>& iat)
+{
+    if (iat.has_value())
+        claims["iat"] = iat->count();
+    else if (claims.contains("iat"))
+        claims.erase("iat");
+}
+
+std::optional<std::chrono::seconds> Jwt::getExp() const
+{
+    if (!claims.contains("exp"))
+        return std::nullopt;
+    return std::chrono::seconds(claims.at("exp").get<int64_t>());
+}
+
+void Jwt::setExp(const std::optional<std::chrono::seconds>& exp)
+{
+    if (exp.has_value())
+        claims["exp"] = exp->count();
+    else if (claims.contains("exp"))
+        claims.erase("exp");
+}
+
+std::optional<std::string> Jwt::getSub() const
+{
+    if (!claims.contains("sub"))
+        return std::nullopt;
+    return claims.at("sub").get<std::string>();
+}
+
+void Jwt::setSub(const std::optional<std::string>& sub)
+{
+    if (sub.has_value())
+        claims["sub"] = sub.value();
+    else if (claims.contains("sub"))
+        claims.erase("sub");
+}
+
+bool Jwt::parse(const std::string& token, const std::string& pem_cert)
 {
     std::vector<std::string> parts = utils::string::split(token, '.');
     if (parts.size() != 3)
     {
+        LOG(ERROR, LOG_TAG) << "Token '" << token << "' not in the format <header>.<payload>.<signature>\n";
         return false;
     }
 
-    LOG(INFO, LOG_TAG) << "Header: " << base64url_decode(parts[0]) << "\n";
-    LOG(INFO, LOG_TAG) << "Payload: " << base64url_decode(parts[1]) << "\n";
+    std::string header = base64url_decode(parts[0]);
+    std::string payload = base64url_decode(parts[1]);
+    LOG(DEBUG, LOG_TAG) << "Header: " << header << ", payload: " << payload << "\n";
 
-    header_ = json::parse(base64url_decode(parts[0]));
-    payload_ = json::parse(base64url_decode(parts[1]));
-    std::string signature = parts[2];
+    try
+    {
+        json jheader = json::parse(header);
+        claims = json::parse(payload);
+        std::string signature = parts[2];
+        LOG(INFO, LOG_TAG) << "Header: " << jheader << "\n";
+        LOG(INFO, LOG_TAG) << "Payload: " << claims << "\n";
+        LOG(INFO, LOG_TAG) << "Signature: " << signature << "\n";
+        auto binary = base64url_decode(signature);
+        std::string msg = parts[0] + "." + parts[1];
+        bool auth;
+        if (!verifySignature(pem_cert, reinterpret_cast<unsigned char*>(binary.data()), binary.size(), msg.c_str(), msg.size(), auth))
+        {
+            LOG(ERROR, LOG_TAG) << "Failed to verify signature\n";
+            return false;
+        }
+        if (!auth)
+        {
+            LOG(ERROR, LOG_TAG) << "Wrong signature\n";
+            return false;
+        }
+        return true;
+    }
+    catch (std::exception& e)
+    {
+        LOG(ERROR, LOG_TAG) << "Error parsing JWT header or payload: " << e.what() << "\n";
+    }
+    return false;
+}
 
-    LOG(INFO, LOG_TAG) << "Header: " << header_ << "\n";
-    LOG(INFO, LOG_TAG) << "Payload: " << payload_ << "\n";
-    LOG(INFO, LOG_TAG) << "Signature: " << signature << "\n";
 
-    return true;
+std::optional<std::string> Jwt::getToken(const std::string& pem_key) const
+{
+    json header = {{"typ", "JWT"}};
+    if (pem_key.find("-----BEGIN PRIVATE KEY-----") == 0)
+        header["alg"] = "RS256";
+    // if (pem_key.find("-----BEGIN EC PRIVATE KEY-----") == 0)
+    //     header["alg"] = "ES256";
+    else
+    {
+        LOG(ERROR, LOG_TAG) << "PEM key must be an RSA key\n";
+        return std::nullopt;
+    }
+
+    LOG(DEBUG, LOG_TAG) << "Header: " << header << ", payload: " << claims << "\n";
+    std::string msg = base64url_encode(header.dump()) + "." + base64url_encode(claims.dump());
+    LOG(DEBUG, LOG_TAG) << "Encoded: " << msg << "\n";
+
+    std::vector<unsigned char> encoded;
+    if (sign(pem_key, msg, encoded))
+    {
+        std::string signature = base64url_encode(encoded.data(), encoded.size());
+        LOG(DEBUG, LOG_TAG) << "Signature: " << signature << "\n";
+        auto token = msg + "." + signature;
+        LOG(DEBUG, LOG_TAG) << "Token: " << token << "\n";
+        return token;
+    }
+
+    LOG(ERROR, LOG_TAG) << "Failed to sign token\n";
+    return std::nullopt;
 }
