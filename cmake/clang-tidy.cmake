10755013fd2104769041afea00634e3c5b6d915f
badaix <johannes.pohl@badaix.de>
2025-02-09 19:56:32 +0100

    Use as many threads as cores for clang-tidy

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -6,9 +6,12 @@ if(CMAKE_VERSION VERSION_EQUAL "3.19.0" OR CMAKE_VERSION VERSION_GREATER
   # If run-clang-tidy found
   if(CLANG_TIDY)
 
+    include(ProcessorCount)
+    ProcessorCount(CPU_CORES)
+
     add_custom_target(
       clang-tidy
-      COMMAND ${CLANG_TIDY}
+      COMMAND ${CLANG_TIDY} -j ${CPU_CORES}
       WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
       COMMENT "Static code analysis using ${CLANG_TIDY}")
 
