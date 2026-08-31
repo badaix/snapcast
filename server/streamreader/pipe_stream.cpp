f8a0389bd71b5a90069f800eeff51d0d0058422f
rwx <rw@witchly.org>
2026-08-31 12:19:52 +0200

    snapserver: add openbsd support

--- server/streamreader/pipe_stream.cpp
+++ server/streamreader/pipe_stream.cpp
@@ -73,7 +73,7 @@ void PipeStream::connect()
     }
 
     int pipe_size = -1;
-#if !defined(MACOS) && !defined(FREEBSD)
+#if !defined(MACOS) && !defined(FREEBSD) && !defined(OPENBSD)
     pipe_size = fcntl(fd, F_GETPIPE_SZ);
 #endif
     LOG(TRACE, LOG_TAG) << "Stream: " << name_ << ", connect to pipe: " << uri_.path << ", fd: " << fd << ", pipe size: " << pipe_size << "\n";
